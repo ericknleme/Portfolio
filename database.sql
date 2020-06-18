83c894d19599cb2a2e0d7e50d7f13a37370205ed
Erick Leme <erick.leme@bandtec.com.br>
2020-06-18 14:03:01 -0300

    Database atualizada

--- database.sql
+++ database.sql
@@ -4,11 +4,8 @@ use portfolio;
 create table usuario(
 id int primary key auto_increment,
 nome varchar(45),
-login varchar(45)
+login varchar(45),
 senha varchar(45)
 );
 
 select * from usuario;
-delete from usuario where id_usuario = 1;
-
-insert into usuario (nome_usuario) values ('Erick');
